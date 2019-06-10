8c0092107c44f5dff83e65ba40bb8893cb14280c
Matt Bierner <matb@microsoft.com>
2019-06-10 15:02:18 -0700

    Handle publish fails due to same version

--- build/bumpAndPublish.sh
+++ build/bumpAndPublish.sh
@@ -3,10 +3,20 @@ npm run bump-version
 result=$?
 
 if [ $result -eq 0 ]; then
-    npx vsce publish $f --pat $1 --noVerify
-    exit
+    output="$(npx vsce publish $f --pat $1 --noVerify 2>&1)"
+    publishResult=$?
+    echo $output
+    if [ $publishResult -eq 0 ]; then
+        exit
+    elif [[ $output =~ "already exists" ]]; then
+        echo "Publish failed but expected. Skipping publishing same version."
+        exit
+    else
+        echo "Error publishing"
+        exit 1
+    fi;
 elif [ $result -eq 1 ]; then
-    echo "Skipping publishing new version"
+    echo "Skipping publishing same version"
     exit
 else
     echo "Error. Exited with $result"
