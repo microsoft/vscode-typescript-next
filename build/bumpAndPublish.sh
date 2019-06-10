bf7d21fc7dad0abaaf396873ca252baa3d7b6099
Matt Bierner <matb@microsoft.com>
2019-06-10 14:23:12 -0700

    Split out script

---
+++ build/bumpAndPublish.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+npm run bump-version
+result=$?
+
+if [ $result -eq 0 ]; then
+    npx vsce publish $f --pat "$(marketplace_key)" --noVerify
+    exit
+elif [ $result -eq 1 ]; then
+    echo "Skipping publishing new version"
+    exit
+else
+    echo "Error. Exited with $result"
+    exit 1
+fi
