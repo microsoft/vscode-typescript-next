11c8143310b698e49b9ba8b0795a67633fd1338e
Matt Bierner <matb@microsoft.com>
2021-03-01 17:04:23 -0800

    Copy patching of JS/TS grammar to match VS Code

--- README.md
+++ README.md
@@ -10,3 +10,5 @@ To make sure you are using `typescript@next`:
 1. Open a JavaScript or TypeScript file in VS Code.
 1. In the VS Code command palette, run the `TypeScript: Select TypeScript version` command.
 1. Make sure you have `Use VS Code's version selected`
+
+Note that this extension also includes the [latest JavaScript and TypeScript grammar](https://github.com/microsoft/TypeScript-TmLanguage).
--- build/updateGrammar.js
+++ build/updateGrammar.js
@@ -11,17 +11,61 @@ var plist = require('fast-plist');
 var https = require('https');
 var url = require('url');
 
-function adaptToJavaScript(grammar) {
+function removeDom(grammar) {
+    grammar.repository['support-objects'].patterns = grammar.repository['support-objects'].patterns.filter(pattern => {
+        if (pattern.match && pattern.match.match(/\b(HTMLElement|ATTRIBUTE_NODE|stopImmediatePropagation)\b/g)) {
+            return false;
+        }
+        return true;
+    });
+    return grammar;
+}
+
+function removeNodeTypes(grammar) {
+    grammar.repository['support-objects'].patterns = grammar.repository['support-objects'].patterns.filter(pattern => {
+        if (pattern.name) {
+            if (pattern.name.startsWith('support.variable.object.node') || pattern.name.startsWith('support.class.node.')) {
+                return false;
+            }
+        }
+        if (pattern.captures) {
+            if (Object.values(pattern.captures).some(capture =>
+                capture.name && (capture.name.startsWith('support.variable.object.process')
+                    || capture.name.startsWith('support.class.console'))
+            )) {
+                return false;
+            }
+        }
+        return true;
+    });
+    return grammar;
+}
+
+function patchJsdoctype(grammar) {
+    grammar.repository['jsdoctype'].patterns = grammar.repository['jsdoctype'].patterns.filter(pattern => {
+        if (pattern.name && pattern.name.indexOf('illegal') >= -1) {
+            return false;
+        }
+        return true;
+    });
+    return grammar;
+}
+
+function patchGrammar(grammar) {
+    return removeNodeTypes(removeDom(patchJsdoctype(grammar)));
+}
+
+function adaptToJavaScript(grammar, replacementScope) {
     grammar.name = 'JavaScript (with React support)';
-    grammar.fileTypes = ['.js', '.jsx'];
-    grammar.scopeName = 'source.js';
+    grammar.fileTypes = ['.js', '.jsx', '.es6', '.mjs', '.cjs'];
+    grammar.scopeName = `source${replacementScope}`;
 
     var fixScopeNames = function (rule) {
         if (typeof rule.name === 'string') {
-            rule.name = rule.name.replace(/\.tsx/g, '.js');
+            rule.name = rule.name.replace(/\.tsx/g, replacementScope);
         }
         if (typeof rule.contentName === 'string') {
-            rule.contentName = rule.contentName.replace(/\.tsx/g, '.js');
+            rule.contentName = rule.contentName.replace(/\.tsx/g, replacementScope);
         }
         for (var property in rule) {
             var value = rule[property];
@@ -36,7 +80,6 @@ function adaptToJavaScript(grammar) {
         fixScopeNames(repository[key]);
     }
 }
-
 
 function getCommitSha(branchId) {
     var commitInfo = 'https://api.github.com/repos/Microsoft/TypeScript-TmLanguage/branches/' + branchId;
@@ -108,7 +151,8 @@ exports.update = function (branchId, repoPath, dest, modifyGrammar) {
     }, console.error);
 }
 
-var branch = process.argv[2] || "main";
-exports.update(branch, 'TypeScript.tmLanguage', './syntaxes/TypeScript.tmLanguage.json');
-exports.update(branch, 'TypeScriptReact.tmLanguage', './syntaxes/TypeScriptReact.tmLanguage.json');
-exports.update(branch, 'TypeScriptReact.tmLanguage', './syntaxes/JavaScript.tmLanguage.json', adaptToJavaScript);
+const branch = "master";
+exports.update(branch, 'TypeScript.tmLanguage', './syntaxes/TypeScript.tmLanguage.json', grammar => patchGrammar(grammar));
+exports.update(branch, 'TypeScriptReact.tmLanguage', './syntaxes/TypeScriptReact.tmLanguage.json', grammar => patchGrammar(grammar));
+exports.update(branch, 'TypeScriptReact.tmLanguage', './syntaxes/JavaScript.tmLanguage.json', grammar => adaptToJavaScript(patchGrammar(grammar), '.js'));
+exports.update(branch, 'TypeScriptReact.tmLanguage', './syntaxes/JavaScriptReact.tmLanguage.json', grammar => adaptToJavaScript(patchGrammar(grammar), '.js.jsx'));
