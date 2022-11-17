020a38621b628304f332870d50c57da8c0fc1368
Matt Bierner <matb@microsoft.com>
2022-11-17 08:46:22 -0800

    Make VERSION_SUFFIX parameter optional
    Apparently `default: ""` is means that the parameter is still required

--- build/updatePackageVersion.js
+++ build/updatePackageVersion.js
@@ -6,7 +6,7 @@ const json = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'package.json
 const jsonLock = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'package-lock.json')).toString());
 
 // Set version to TS Version
-const versionSuffix = process.argv[2] || '';
+const versionSuffix = (process.argv[2] || '').trim();
 const version = jsonLock['dependencies']['typescript']['version'].replace(/0?\-\w*\./g, '') + versionSuffix;
 if (version === json['version']) {
     console.log(`Already at latest version ${version}`);
